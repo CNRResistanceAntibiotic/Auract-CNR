30fcb3ed9583e22b73156bb3029ff798c5496d77
Nilad <lezebre9@hotmail.fr>
2021-11-08 15:29:04 +0100

    convert

--- auract/microreact.py
+++ auract/microreact.py
@@ -104,12 +104,18 @@ class Microreact(Dataset):
         log("Processing Microreact api request", type='debug')
         try:
             log()
+
             result = subprocess.check_output(
-                'curl -H "Content-type: application/json; charset=UTF-8"'
-                ' -H  "Access-Token: eyJhbGciOiJIUzUxMiJ9.eyJpZCI6IjVmZjZkNjFiYjU2NGRlMDY1ZTNhMDdkMyIsImlhdCI6MTYzNjM3Nzk1MSwiZXhwIjoxNzk0MDU3OTUxfQ.U4o6BnYXUghxjyEyc2DPIHEe9qLMSdl_Q_yvgF4B23JsCTvU0ZRjXTHbnsinKTQ2fVfGsSjvIdoZbiQgtZXCiQ"'
-                ' -X POST'
-                ' -d @' + jsonpath +
-                ' https://microreact.org/api/projects/create', shell=True)
+                'curl --header "Content-type: application/json; charset=UTF-8"'
+                ' --request POST'
+                ' --data '+jsonpath +
+                ' https://demo.microreact.org/api/schema/convert'
+                ' | '
+                'curl --header "Content-type: application/json; charset=UTF-8"'
+                ' --header "Access-Token: eyJhbGciOiJIUzUxMiJ9.eyJpZCI6IjVmZjZkNjFiYjU2NGRlMDY1ZTNhMDdkMyIsImlhdCI6MTYzNjM4MTA0NywiZXhwIjoxNzk0MDYxMDQ3fQ.Y6I_Z6i2AO8YvgM_Wu9DzOUGZbKBywKzDUusDLedgQCHeoRaS9ydMpQNnTb6esijBlNicDb6u4ySbdcxkkdTAw"'
+                ' --data @-'
+                ' https://microreact.org/api/projects/create'
+                , shell=True)
             api_result = json.loads(result)
             log('Microreact link : ' + api_result['url'])
             log()
